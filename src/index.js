8d0dc0651dc0ad19b82df5798c0dcaee32a45e6f
Blake <bsward8@gmail.com>
2019-02-05 16:28:13 +1100

    feat: ghostbuster implementation

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const Plugin = require('powercord/Plugin');
+const { getModule, instance } = require('powercord/webpack');
+const { create: createMessage } = getModule(m => m.prototype && m.prototype.updateMessage);
+const { exports: MessagesPopout } = instance.cache[6356];
+
+module.exports = class GhostBuster extends Plugin {
+  async start () {
+    this.messages = [];
+
+    const mdl = getModule(['makeTextChatNotification']);
+    mdl.shouldNotify = (_shouldNotify => (...args) => {
+      const wasMentioned = _shouldNotify(...args);
+      if (wasMentioned) {
+        this.messages.push(createMessage(args[0]));
+      } 
+      return wasMentioned;
+    })(mdl.shouldNotify);
+
+    const _this = this;
+    MessagesPopout.prototype.render = (_render => function(...args) {
+      if (this.props.analyticsName === 'Recent Mentions') {
+        if (this.props.messages) {
+          this.props.messages = [...new Set(this.props.messages)]
+            .concat(_this.messages)
+            .sort((msg, prev) => prev.timestamp._d - msg.timestamp._d);
+        } else {
+          this.props.messages = _this.messages;
+        }
+      }
+      return _render.call(this, ...args);
+    })(MessagesPopout.prototype.render);
+  }
+};
