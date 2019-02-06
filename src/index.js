c176c4506f27bde628a96bb6abed3534620fa428
Blake <bsward8@gmail.com>
2019-02-07 07:03:38 +1100

    fix: add support for DND using isMentioned

--- src/index.js
+++ src/index.js
@@ -1,33 +1,45 @@
 const Plugin = require('powercord/Plugin');
-const { getModule, instance } = require('powercord/webpack');
+
+const { getModule, getModuleByDisplayName } = require('powercord/webpack');
 const { create: createMessage } = getModule(m => m.prototype && m.prototype.updateMessage);
-const { exports: MessagesPopout } = instance.cache[6356];
+
+const MessagesPopout = getModuleByDisplayName('MessagesPopout');
+
+const { inject } = require('powercord/injector');
+
+const { isMentioned } = getModule([ 'isMentioned' ]);
+const { getCurrentUser } = getModule([ 'getCurrentUser' ]);
 
 module.exports = class GhostBuster extends Plugin {
   async start () {
     this.messages = [];
 
-    const mdl = getModule(['makeTextChatNotification']);
-    mdl.shouldNotify = (_shouldNotify => (...args) => {
-      const wasMentioned = _shouldNotify(...args);
-      if (wasMentioned) {
-        this.messages.push(createMessage(args[0]));
-      } 
-      return wasMentioned;
-    })(mdl.shouldNotify);
+    // Thanks Joakim!
+    const mdl = getModule([ 'shouldNotify' ]);
+    inject('pc-ghostbuster-shouldNotify', mdl, 'shouldNotify', (args, res) => {
+      const self = getCurrentUser();
+      const message = args[0];
+
+      if (self.id !== message.author.id && isMentioned(message, self.id, true)) {
+        this.messages.push(createMessage(message));
+      }
+      
+      return res;
+    });
 
     const _this = this;
-    MessagesPopout.prototype.render = (_render => function(...args) {
+    MessagesPopout.prototype.render = (_render => function (...args) {
       if (this.props.analyticsName === 'Recent Mentions') {
         if (this.props.messages) {
           this.props.messages = this.props.messages
             .concat(_this.messages)
-            .sort((msg, prev) => prev.timestamp._d - msg.timestamp._d)
-            .reduce((prev, curr) => prev.find(a => a.id === curr.id) ? prev : prev.push(curr) && prev, [])
+            .filter((value, index, self) => self.findIndex(v => v.id === value.id) === index)
+            .sort((msg, prev) => prev.timestamp._d - msg.timestamp._d);
         } else {
           this.props.messages = _this.messages;
         }
       }
+
       return _render.call(this, ...args);
     })(MessagesPopout.prototype.render);
   }
