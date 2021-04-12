13480a45fe73fa35dc870f960591084f1a538ca0
EpicPerson <54505527+metoo67789@users.noreply.github.com>
2021-04-12 10:48:26 -0400

    fix

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ module.exports = class GhostBuster extends Plugin {
 
     const { isMentioned } = await getModule([ 'isMentioned' ]);
     const { getCurrentUser } = await getModule([ 'getCurrentUser' ]);
-    const { create: createMessage } = await getModule(m => m.prototype && m.prototype.updateMessage);
+    const Message = await getModule(m => m.prototype && m.prototype.getReaction && m.prototype.isSystemDM);
+		const Author = await getModule(m => m.prototype && m.prototype.getAvatarURL && m.prototype.isStaff);
+		const Timestamp = await getModule(m => m.prototype && m.prototype.add && m.prototype.toDate);
 
     // Thanks Joakim!
     const mdl = await getModule([ 'shouldNotify' ]);
@@ -16,26 +18,28 @@ module.exports = class GhostBuster extends Plugin {
       const self = getCurrentUser();
       const message = args[0];
 
-      if (self.id !== message.author.id && isMentioned(message, self.id, true)) {
-        this.messages.push(createMessage(message));
-      }
+      if (self.id !== message.author.id && isMentioned(self.id, message.channel_id, null, message.content, null, true)) {
+				message.timestamp = new Timestamp(message.timestamp);
+				message.author = new Author(message.author);
+				this.messages.push(new Message(message));
+			}
 
       return res;
     });
 
-    const _this = this;
-    const MessagesPopout = (await getModuleByDisplayName('FluxContainer(MessagesPopout)'));
-    inject('ghostbuster-render', MessagesPopout.prototype, 'render', function (args) {
-      if (this.props.analyticsName === 'Recent Mentions') {
-        if (this.props.messages) {
-          this.props.messages = this.props.messages
-            .concat(_this.messages)
-            .filter((value, index, self) => self.findIndex(v => v.id === value.id) === index)
-            .sort((msg, prev) => prev.timestamp._d - msg.timestamp._d);
-        } else {
-          this.props.messages = _this.messages;
-        }
-      }
+		const _this = this;
+		const MessagesPopout = getModule(m => m.default?.displayName === 'MessagesPopout', false);
+		inject('ghostbuster-render', MessagesPopout, 'default', function (args) {
+				if (args[0].analyticsName === 'Recent Mentions') {
+					if (args[0].messages) {
+						args[0].messages = args[0].messages
+							.concat(_this.messages)
+							.filter((value, index, self) => self.findIndex(v => v.id === value.id) === index)
+							.sort((msg, prev) => prev.timestamp._d - msg.timestamp._d);
+					} else {
+						args[0].messages = _this.messages;
+					}
+				}
 
       return args;
     }, true);
